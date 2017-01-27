cac0118db43a72b23477c08cd167bc8d1ac08325
Jacinda <jacindaz@gmail.com>
2017-01-26 16:18:06 -0800

    Set up creating a new room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,9 +4,23 @@ class RoomsController < ApplicationController
   end
 
   def new
-    binding.pry
+    @room = Room.new
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.create
+      flash[:message] = "Room saved."
+      redirect_to rooms_path
+    else
+      flash[:message] = "Room failed to save."
+      render new_room_path(@room)
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
