ead188c5f3a5d2787fc6964cfed8ebe4670880a7
Jacinda <jacindaz@gmail.com>
2017-01-27 16:00:43 -0800

    Updates for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # for devise
+  root to: "rooms#index"
+
   resources :rooms
   resources :posts
 end
